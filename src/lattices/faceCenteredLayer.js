01156b0a7b1d7b74896a45fb68ef7d6f327f2692
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2019-12-16 18:21:46 -0800

    Almost done with FCC layering

--- src/lattices/faceCenteredLayer.js
+++ src/lattices/faceCenteredLayer.js
@@ -4,14 +4,14 @@ function v(x, y, z) {
   return vec3.fromValues(1.4*x - 1.4*3, 1.4*y - 1.4*3, 1.4*z - 1.4*3);
 }
 
-const allSpheres = [];
-/*for (let y = 0; y < 4; y++) {
+let allSpheres = [];
+for (let y = 0; y < 4; y++) {
   for (let x = 0; x < 4; x++) {
     for (let z = 0; z < 4; z++) {
-      allSpheres.push(v(2*x, 2*y, 2*z));
+      allSpheres.push(v(2*x-1, 2*y-1, 2*z));
     }
   }
-}*/
+}
 
 for (let y = 0; y < 3; y++) {
   for (let x = 0; x < 3; x++) {
@@ -37,7 +37,7 @@ for (let y = 0; y < 3; y++) {
   }
 }
 
-const layers = [
+let layers = [
   [],
   [],
   [],
@@ -50,19 +50,22 @@ const layers = [
   []
 ];
 
-//FCC cell layering
-function FaceCenteredLayer(restHeight, sphere, totalLayerCount, layerIndex, color, expan) {
+const planeNormal = vec3.fromValues(1,1,1);
+for (const pos of allSpheres) {
+  for (let i = 0; i < 10; i++) {
+    const pointOnPlane = vec3.fromValues(2.8*i - 1.4*3,-1.4*3,-1.4*3);
+    const d = -vec3.dot(planeNormal, pointOnPlane);
+    if (Math.abs(vec3.dot(planeNormal, pos) - d) < 0.01) {
+      layers[9-i].push(pos);
+    }
+  }
+}
 
-    this.reset = function() {
-        curHeight = startHeight;
-        atRest = false;
-    };
+//FCC cell layering
+function FaceCenteredLayer(sphere, totalLayerCount, layerIndex, color, expan) {
 
     this.update = function(t, i) {
       this.hidden = (t === this.startHeight && i !== 0);
-      if (t === this.restHeight) {
-        this.atRest = true;
-      }
       curHeight = t;
     };
 
@@ -79,25 +82,26 @@ function FaceCenteredLayer(restHeight, sphere, totalLayerCount, layerIndex, colo
     };
 
     this.draw = function(MV, prog) {
-        //if (this.hidden) return;
+        if (this.hidden) return;
 
         gl.uniform1f(prog.getHandle("alpha"), 1.0);
         gl.uniform3fv(prog.getHandle("kdFront"), color);
 
-        for (const pos of /*layers[layerIndex]*/allSpheres) {
+        MV.pushMatrix();
+        MV.translate(vec3.fromValues(curHeight, curHeight, curHeight));
+        for (const pos of layers[layerIndex]) {
           this._drawSphere(MV, prog, pos);
         }
+        MV.popMatrix();
     };
 
-    this.isAtRest = function() { return atRest; };
-
     this.startHeight = 10.0;
-    this.restHeight = restHeight;
+    this.restHeight = 0;
     var curHeight = 10.0;
     var speed = .1;
     var atRest = false;
     var color = color;
-    var offset = vec3.fromValues(0, restHeight ,0);
+    var offset = vec3.fromValues(0, 0 ,0);
     var sphere = sphere;
     var flip = false;
     var calledFlip = false;

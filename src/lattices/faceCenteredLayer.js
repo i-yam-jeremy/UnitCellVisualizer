daf4ade43b8668fc9d23ee47a9c4f809bd3acc5e
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2019-12-16 18:26:31 -0800

    Cleaned up FCC layering

--- src/lattices/faceCenteredLayer.js
+++ src/lattices/faceCenteredLayer.js
@@ -1,67 +1,69 @@
 import {vec3} from '../gl-matrix';
 
-function v(x, y, z) {
-  return vec3.fromValues(1.4*x - 1.4*3, 1.4*y - 1.4*3, 1.4*z - 1.4*3);
-}
+const spherePositionsByLayer = (() => {
+  function v(x, y, z) {
+    return vec3.fromValues(1.4*x - 1.4*3, 1.4*y - 1.4*3, 1.4*z - 1.4*3);
+  }
 
-let allSpheres = [];
-for (let y = 0; y < 4; y++) {
-  for (let x = 0; x < 4; x++) {
-    for (let z = 0; z < 4; z++) {
-      allSpheres.push(v(2*x-1, 2*y-1, 2*z));
+  let allSpheres = [];
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      for (let z = 0; z < 4; z++) {
+        allSpheres.push(v(2*x-1, 2*y-1, 2*z));
+      }
     }
   }
-}
 
-for (let y = 0; y < 3; y++) {
-  for (let x = 0; x < 3; x++) {
-    for (let z = 0; z < 4; z++) {
-      allSpheres.push(v(2*x, 2*y, 2*z));
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+      for (let z = 0; z < 4; z++) {
+        allSpheres.push(v(2*x, 2*y, 2*z));
+      }
     }
   }
-}
 
-for (let y = 0; y < 4; y++) {
-  for (let x = 0; x < 3; x++) {
-    for (let z = 0; z < 3; z++) {
-      allSpheres.push(v(2*x, 2*y-1, 2*z+1));
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 3; x++) {
+      for (let z = 0; z < 3; z++) {
+        allSpheres.push(v(2*x, 2*y-1, 2*z+1));
+      }
     }
   }
-}
 
-for (let y = 0; y < 3; y++) {
-  for (let x = 0; x < 4; x++) {
-    for (let z = 0; z < 3; z++) {
-      allSpheres.push(v(2*x-1, 2*y, 2*z+1));
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 4; x++) {
+      for (let z = 0; z < 3; z++) {
+        allSpheres.push(v(2*x-1, 2*y, 2*z+1));
+      }
     }
   }
-}
 
-let layers = [
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  []
-];
+  let layers = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ];
 
-const planeNormal = vec3.fromValues(1,1,1);
-for (let i = 0; i < 10; i++) {
-  const pointOnPlane = vec3.fromValues(2.8*(i+1) - 1.4*3,-1.4*3,-1.4*3);
-  const d = -vec3.dot(planeNormal, pointOnPlane);
-  for (const pos of allSpheres) {
-    if (Math.abs(vec3.dot(planeNormal, pos) - d) < 0.01) {
-      layers[9-i].push(pos);
+  const planeNormal = vec3.fromValues(1,1,1);
+  for (let i = 0; i < 10; i++) {
+    const pointOnPlane = vec3.fromValues(2.8*(i+1) - 1.4*3,-1.4*3,-1.4*3);
+    const d = -vec3.dot(planeNormal, pointOnPlane);
+    for (const pos of allSpheres) {
+      if (Math.abs(vec3.dot(planeNormal, pos) - d) < 0.01) {
+        layers[9-i].push(pos);
+      }
     }
   }
-}
 
-console.log(layers);
+  return layers;
+})();
 
 //FCC cell layering
 function FaceCenteredLayer(sphere, totalLayerCount, layerIndex, color, expan) {
@@ -91,7 +93,7 @@ function FaceCenteredLayer(sphere, totalLayerCount, layerIndex, color, expan) {
 
         MV.pushMatrix();
         MV.translate(vec3.fromValues(curHeight, curHeight, curHeight));
-        for (const pos of layers[layerIndex]) {
+        for (const pos of spherePositionsByLayer[layerIndex]) {
           this._drawSphere(MV, prog, pos);
         }
         MV.popMatrix();
